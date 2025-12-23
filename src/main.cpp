1b9df59ba20307aee26932b843e8e9006a037c0f
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-12-23 11:58:04 -0600

    Change DummyAddr from constexpr to const

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 extern "C" void DummyFunction() {}
 
-constexpr void* DummyAddr = reinterpret_cast<void*>(&DummyFunction);
+const void* DummyAddr = reinterpret_cast<void*>(&DummyFunction);
 
 MAKE_DLOPEN_HOOK(Test, DummyAddr, void) {
     // This will absolutely crash. It's just a test to make sure the compiler sees the logger
