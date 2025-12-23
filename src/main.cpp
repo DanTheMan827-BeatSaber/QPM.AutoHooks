1ac5ffc887a1c616c10768f4c740a0567ab384db
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-12-23 11:39:15 -0600

    Add DummyFunction to avoid name mangling

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "../shared/hooks.hpp"
 
-MAKE_DLOPEN_HOOK(Test, reinterpret_cast<void*>(0), void) {
+// Dummy function with C linkage to avoid name mangling
+extern "C" void DummyFunction() {
+    // intentionally empty
+}
+
+MAKE_DLOPEN_HOOK(Test, &DummyFunction, void) {
     // This will absolutely crash. It's just a test to make sure the compiler sees the logger
 }
