09e82e024b3a1b179405ea7045f14298bd36ad6b
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-12-23 11:50:30 -0600

    Remove DummyFunction and update MAKE_DLOPEN_HOOK

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include "../shared/hooks.hpp"
 
-// Dummy function with C linkage to avoid name mangling
-extern "C" void DummyFunction() {
-    // intentionally empty
-}
-
-MAKE_DLOPEN_HOOK(Test, reinterpret_cast<void*>(&DummyFunction), void) {
+MAKE_DLOPEN_HOOK(Test, 0x1, void) {
     // This will absolutely crash. It's just a test to make sure the compiler sees the logger
 }
